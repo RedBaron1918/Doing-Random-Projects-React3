8594f5d5ad0b3bb4ed7003f923726071245def35
RedBaron1918 <asatianikotiko1@gmail.com>
2022-08-20 21:34:25 +0400

    third random  project

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import {useState, useEffect} from 'react'
+import {FiChevronRight, FiChevronLeft} from 'react-icons/fi'
+import {FaQuoteRight} from 'react-icons/fa'
+import data from './data'
+function App() {
+    const [people, setPeople] = useState(data)
+    const [index, setIndex] = useState(0)
+    useEffect(() => {
+        const lastIndex = people.length - 1
+        if(index < 0){
+            setIndex(lastIndex)
+        }
+        if (index >lastIndex) {
+            setIndex(0)
+        }
+    }, [people,index])
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex(index + 1)
+        } , 3000)
+        return () => clearInterval(interval)
+    } , [index])
+
+  return (
+    <section className='section'>
+        <div className='title'>
+            <h2>
+                <span>/</span>reviews
+            </h2>
+        </div>
+        <div className="section-center">
+            {people.map((person, Posindex) => {
+                const {name, quote, image,title,id} = person
+                let position = 'nextSlide'
+                if(Posindex === index){
+                    position = 'activeSlide'
+                }
+                if(Posindex === index - 1 || (index === 0 && Posindex === people.length - 1)){
+                    position = 'lastSlide'
+                }
+                return(
+                        <article className={position} key={id}>
+                            <img src={image} alt={name} className="person-img"/>
+                            <h4>{name}</h4>
+                            <p className='title'>{title}</p>
+                            <p className='text'>{quote}</p>
+                            <FaQuoteRight className='icon'></FaQuoteRight>
+
+                        </article>
+                )
+            }
+            )}
+            <button className='prev' onClick={() => setIndex(index - 1)}><FiChevronLeft></FiChevronLeft></button>
+            <button className='next' onClick={() => setIndex(index + 1)}><FiChevronRight></FiChevronRight></button>
+        </div>
+    </section>
+  )
+}
+
+export default App
